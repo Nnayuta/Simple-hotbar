99bc20f8a2619bf2381da1e858be5864be21771c
nayuta <naayudan@gmail.com>
2021-04-06 08:45:41 -0300

    Hotbar
    Basic hotbar system

---
+++ Assets/hotbarConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class hotbarConfig : MonoBehaviour, IDropHandler
+{
+    public KeyCode HotKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //  Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<hotbarSpell>().hotbar = this.GetComponent<hotbarConfig>();
+        }
+    }
+}
---
+++ Assets/hotbarSpell.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class hotbarSpell : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField]
+    public hotbarConfig hotbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (hotbar != null)
+        {
+            if (Input.GetKeyDown(hotbar.HotKey))
+            {
+                Debug.Log("Spell Castada por: " + hotbar.name);
+            }
+
+        }
+
+    }
+
+    // Pick and Drop
+
+    [Header("Sistema de pega os objetos")]
+
+    [SerializeField] private Canvas canvas;
+   
+    private RectTransform rectTrasform;
+    private CanvasGroup canvasGroup;
+
+    void Awake()
+    {
+        if (gameObject.GetComponent<CanvasGroup>() != null)
+        {
+            this.gameObject.AddComponent<CanvasGroup>();
+        }
+
+        rectTrasform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //  Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //   Debug.Log("onDrag");
+        rectTrasform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //   Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+}
